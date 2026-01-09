5398eb51dc63bbb34a24930b5e138fd8589d294e
Legnat91 <angelcalle_91@hotmail.com>
2026-01-09 13:42:31 +0100

    modificacions js
    modificaciones en el js, añadido funciones de guardar, modificar y cancelar

--- aplicacion-Notas/app.js
+++ aplicacion-Notas/app.js
@@ -10,30 +10,49 @@ document.addEventListener('DOMContentLoaded', () => {
     const li = document.createElement('li');
     li.classList.add('nota');
 
-    li.innerHTML = `<span>${texto}</span> <button class="btnBorrar">Borrar</button>`;
+    li.innerHTML = `<span class="texto">${texto}</span> 
+    <span><button class="btnModificar">Modificar</button> <button class="btnBorrar">Borrar</button></span>`;
 
     return li;
   }
-  
-  function guardarNotas(){
-    const notasArrays=[];
-    
-    document.querySelectorAll('#notas li span').forEach(span=>{
+
+  function guardarNotas() {
+    const notasArrays = [];
+
+    document.querySelectorAll('#notas li .texto').forEach(span => {
       notasArrays.push(span.textContent);
     });
 
-    localStorage.setItem('notas',JSON.stringify(notasArrays))
+    localStorage.setItem('notas', JSON.stringify(notasArrays))
 
   }
 
-  function cargarNotas(){
-    const notasGuardadas=JSON.parse(localStorage.getItem('notas'))||[];
-    notasGuardadas.forEach(texto=>{
-      const li=ingresarNota(texto);
+  function cargarNotas() {
+    const notasGuardadas = JSON.parse(localStorage.getItem('notas')) || [];
+    notasGuardadas.forEach(texto => {
+      const li = ingresarNota(texto);
       notas.append(li);
     })
   }
 
+  function modificar(target) {
+    const li = target.closest("li");
+    const textoOriginal = li.querySelector(".texto").textContent;
+    return li.innerHTML = `<input type="text" class="inputMod" value="${textoOriginal}">
+    <span><button class="btnGuardar" type="button">Guardar</button> <button class="btnCancelar" type="button">Cancelar</button></span>`
+  }
+
+  function guardarMod(target) {
+    const li = target.closest('li');
+    const nuevoTexto = li.querySelector('.inputMod').value.trim();
+
+    if (nuevoTexto === "") return;
+
+    li.innerHTML = `
+    <span class="texto">${nuevoTexto}</span> 
+    <span><button class="btnModificar">Modificar</button> <button class="btnBorrar">Borrar</button></span>
+  `;
+  }
 
   btnAgregar.addEventListener('click', () => {
 
@@ -72,10 +91,18 @@ document.addEventListener('DOMContentLoaded', () => {
       const li = target.closest("li");
       li.remove();
       guardarNotas();
+    } else if (target.classList.contains('btnModificar')) {
+      modificar(target);
+    } else if (target.classList.contains('btnCancelar')) {
+      notas.innerHTML = "";
+      cargarNotas();
+    } else if (target.classList.contains('btnGuardar')) {
+      guardarMod(target);
+      guardarNotas();
     }
 
   })
 
- cargarNotas();
+  cargarNotas();
 
 });
