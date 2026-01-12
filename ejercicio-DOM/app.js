4cf7adc0010817bebbe8a6947cb75f720e514935
Legnat91 <angelcalle_91@hotmail.com>
2026-01-12 18:49:26 +0100

    cambios en ejercicio DOM
    cambios para que cuando envie el mensaje y cargue la paguina aparezca el ultimo mensaje enviado

--- ejercicio-DOM/app.js
+++ ejercicio-DOM/app.js
@@ -15,42 +15,45 @@ document.addEventListener('DOMContentLoaded', () => {
       mensaje: mensaje.value.trim()
     };
 
-
     preview.classList.add('vista-previa');
-
-  }
-
-  formulario.addEventListener('submit', (e) => {
-
-    e.preventDefault();
-    
-    const accion = e.submitter.value;
-
-    if (accion === 'preview') {
-      mensajeTexto();
-      preview.innerHTML = `<div>
+    preview.innerHTML = `<div>
     <p><strong>Nombre:</strong> ${datosMensaje.nombre}</p> 
     <p><strong>E-mail:</strong> ${datosMensaje.email}</p> 
     <p><strong>Mensaje:</strong> </p>
     <p class="mensaje-previo">${datosMensaje.mensaje}</p>
-    </div>
-    <div class="botones">
-    <button class="btn-enviar" type="submit" name="accion" value="enviar">Enviar</button>
-    </div>
+    </div>    
     `;
+    localStorage.setItem('mensajeGuardado', JSON.stringify(datosMensaje));
+    alert('Mensaje enviado y guardado');
+    formulario.reset();
+  }
+  
+  function mostrarPreview(datos) {
+  preview.classList.add('vista-previa');
+  preview.innerHTML = `
+    <div>
+      <p><strong>Nombre:</strong> ${datos.nombre}</p> 
+      <p><strong>E-mail:</strong> ${datos.email}</p> 
+      <p><strong>Mensaje:</strong></p>
+      <p class="mensaje-previo">${datos.mensaje}</p>
+    </div>
+  `;
+}
 
+  function cargarMensaje() {
+    const datosGuardados = JSON.parse(localStorage.getItem('mensajeGuardado'));
+    if (datosGuardados) {
+      mostrarPreview(datosGuardados);
     }
 
+  }
 
-    if (accion === 'enviar') {
-      localStorage.setItem('mensajeGuardado', JSON.stringify(datosMensaje));
-      alert('Mensaje enviado y guardado');
-      formulario.reset();
-      preview.innerHTML = ``;
-      preview.classList.remove('vista-previa');
-    }
 
+  formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    mensajeTexto();
   });
 
-  
+  cargarMensaje()
+
 });
